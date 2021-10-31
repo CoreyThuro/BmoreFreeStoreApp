eabdc77c2ea7beb2cd15bb734870005b15737a3a
CoreyThuro <corey.thuro@gmail.com>
2021-10-31 19:16:22 -0400

    Updates

---
+++ precache-manifest.9dccf4cccaad627f33bd4fa8ac6c9493.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6896d2be65dc7d9c2a9bdf2c63619bdc",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/index.html"
+  },
+  {
+    "revision": "81bff75c68362c547e0a",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/css/2.f6aa8dcc.chunk.css"
+  },
+  {
+    "revision": "45485b91a658b527a27a",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/css/main.5670a3a3.chunk.css"
+  },
+  {
+    "revision": "81bff75c68362c547e0a",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/js/2.c284220e.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/js/2.c284220e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45485b91a658b527a27a",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/js/main.4e589f23.chunk.js"
+  },
+  {
+    "revision": "731b8e01576a0de05b05",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/js/runtime-main.3b31c1ab.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "02bba601716f9a0ff1e241a64cfe9b29",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/lobster-latin-400.02bba601.woff"
+  },
+  {
+    "revision": "fe012fb7b22e5776c4a2e91889068974",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/lobster-latin-400.fe012fb7.woff2"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/CoreyThuro/BmoreFreeStoreApp/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  }
+]);
